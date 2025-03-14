d44d83b5ebaa50d308f678bcdc97f10fc277be1b
Baimanchick <baikaimanchick@gmail.com>
2025-03-14 17:08:19 +0600

    [FIX] fixed build error

--- src/shared/types/favorite-types/favorite.ts
+++ src/shared/types/favorite-types/favorite.ts
@@ -1,3 +1,5 @@
+import { ProductTypes } from '../products/ProductsTypes'
+
 export namespace FavoritesType {
     export interface Form {
         product: string | string[] | undefined
@@ -12,28 +14,11 @@ export namespace FavoritesType {
         slug: string;
         title: string;
     }
-
-    export interface Product {
-        slug: string;
-        category: Category;
-        title: string;
-        description: string;
-        base_price: string;
-        main_image: string;
-        article: string;
-        produced: string;
-        composition: string;
-        created_at: string;
-        collection: string;
-        in_favorite: boolean;
-        in_cart: boolean;
-    }
-
     export interface Item {
         id: any;
         created_at: string;
         user: User;
-        product: Product;
+        product: ProductTypes.Item;
     }
 
     export interface ApiResponse {
